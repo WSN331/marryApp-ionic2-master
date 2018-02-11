9b21a2ae40f96d9bb1c680063d7d40750b9943ce
qiujianhui <1224565990@qq.com>
2018-02-11 22:27:12 +0800

    2018-2-10 小红娘页创建

--- src/pages/matchmaker/matchmaker.ts
+++ src/pages/matchmaker/matchmaker.ts
@@ -6,6 +6,7 @@ import * as AV from "leancloud-realtime"
 import {Memory} from "../../util/Memory";
 import {MyHttp} from "../../util/MyHttp";
 import {ImgService} from "../../util/ImgService";
+import {Http} from "@angular/http";
 
 
 @Component({
@@ -17,7 +18,7 @@ export class MatchmakerPage{
 
   constructor(public navCtrl:NavController,public navParams:NavParams,public memory:Memory,
               public events: Events,public alertCtrl:AlertController,private myHttp:MyHttp,
-              public imgService: ImgService) {
+              public imgService: ImgService,private http:Http) {
 
   };
 
